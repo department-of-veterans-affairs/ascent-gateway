d6ff0831312cea2e40f0eba8dbc44537bd07ee6f
suchitharavi <sravi@ip-192-168-47-70.us-gov-west-1.compute.internal>
2018-07-18 12:38:29 -0400

    plat-256 updated apputil to get baseurl method from test framework

--- src/inttest/java/gov/va/ascent/gateway/util/GatewayAppUtil.java
+++ src/inttest/java/gov/va/ascent/gateway/util/GatewayAppUtil.java
@@ -1,47 +1,16 @@
 package gov.va.ascent.gateway.util;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import gov.va.ascent.test.framework.service.RESTConfigService;
-import gov.va.ascent.test.framework.service.VaultService;
-import gov.va.ascent.test.framework.util.AppConstants;
-import gov.va.ascent.test.framework.util.RESTUtil;
 
 public class GatewayAppUtil {
-
-	private static final Pattern urlPattern = Pattern.compile("(http|https)://([A-Za-z0-9\\-\\.]+)(:(\\d+))?$");
-
+	
 	private GatewayAppUtil() {
 
 	}
-
+	
 	public static String getBaseURL() {
-		RESTConfigService restConfig = RESTConfigService.getInstance();
-		String baseURL = restConfig.getPropertyName("baseURL", true);
-
-		String vaultToken = System.getProperty(AppConstants.VAULT_TOKEN_PARAM_NAME);
-		if (vaultToken != null && vaultToken != "") {
-			String jsonResponse = VaultService.getVaultCredentials(vaultToken);
-
-			RESTUtil restUtil = new RESTUtil();
-			String userName = restUtil.parseJSON(jsonResponse, "data.'ascent.security.username'");
-			String password = restUtil.parseJSON(jsonResponse, "data.'ascent.security.password'");
-
-			final Matcher m = urlPattern.matcher(baseURL);
-			if (!m.matches())
-				throw new RuntimeException("Invalid base url!");
-			final String protocol = m.group(1).toLowerCase();
-			final String host = m.group(2);
-			final String port     = m.group(3);
-			StringBuffer urlBuffer = new StringBuffer();
-			urlBuffer.append(protocol).append("://").append(userName).append(":").append(password).append("@").append(host);
-			if(port !=null) {
-				urlBuffer.append(port);
-			}
-			
-			return urlBuffer.toString();
-		}
-		return baseURL;
+		return RESTConfigService.getBaseURL("data.'ascent.security.username'", "data.'ascent.security.password'");
 	}
 }
+
+
