119d578ab7561fe6eeff17ae32b8bfdd1f5f3274
armin <aburkholder@meetveracity.com>
2018-07-24 15:19:33 -0400

    PLAT-258 more sonar and fortify issues resolved

--- src/main/java/gov/va/ascent/gateway/error/AscentGatewayErrorController.java
+++ src/main/java/gov/va/ascent/gateway/error/AscentGatewayErrorController.java
@@ -28,29 +28,54 @@ import com.netflix.zuul.context.RequestContext;
 
 import gov.va.ascent.framework.util.SanitizationUtil;
 
+/**
+ * The rest endpoint resource for gateway error handling.
+ *
+ * @author aburkholder
+ */
 @RestController
 public class AscentGatewayErrorController extends AbstractErrorController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(AscentGatewayErrorController.class);
 
+	/** The request mapping */
 	@Value("${error.path:/error}")
 	private String errorPath;
 
+	/** Attributes related to error handling, set on the request object */
 	@Autowired
 	private ErrorAttributes errorAttributes;
 
+	/** Manage sleuth instrumentation spans */
 	@Autowired
 	Tracer tracer;
 
+	/**
+	 * Instantiate the resource endpoint.
+	 *
+	 * @param errorAttributes error handling attributes
+	 */
 	public AscentGatewayErrorController(final ErrorAttributes errorAttributes) {
 		super(errorAttributes);
 	}
 
+	/**
+	 * Get the request mapping path.
+	 *
+	 * @return String the request mapping path
+	 */
 	@Override
 	public String getErrorPath() {
 		return errorPath;
 	}
 
+	/**
+	 * The endpoint operation for error handling.
+	 *
+	 * @param request the HttpServletRequest
+	 * @param response the HttpServletResponse
+	 * @return ResponseEntity&lt;Map&lt;String, Object&gt;&gt; the entity for the response
+	 */
 	@RequestMapping(value = "${error.path:/error}", produces = "application/json;charset=UTF-8")
 	// NOSONAR TODO: Need to review the method to figure out if this is better way to add span error tag and also the response
 	// status body message to be set using getErrorAttributes(request, true)
@@ -65,22 +90,11 @@ public class AscentGatewayErrorController extends AbstractErrorController {
 		int status = response.getStatus();
 		if (status == 0) {
 			status = HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+			LOGGER.error("RESPONSE:: > ResponseEntity status modified from 0 to " + status);
 		}
 
-		final RequestContext ctx = RequestContext.getCurrentContext();
-		String errorMessage = "Unexpected error occurred";
-		final InputStream inputStream = ctx.getResponseDataStream();
+		final String errorMessage = getErrorMessage(RequestContext.getCurrentContext());
 
-		if (inputStream != null) {
-			try {
-				errorMessage = IOUtils.toString(inputStream, StandardCharsets.UTF_8.name());
-				LOGGER.error("Error Response:: {}", errorMessage);
-			} catch (final IOException e) {
-				LOGGER.error("IOException:: {}", e);
-			} finally {
-				IOUtils.closeQuietly(inputStream);
-			}
-		}
 		LOGGER.debug("Tracer Current Span: {}", this.tracer.getCurrentSpan());
 
 		this.tracer.addTag(Span.SPAN_ERROR_TAG_NAME, errorMessage);
@@ -92,6 +106,40 @@ public class AscentGatewayErrorController extends AbstractErrorController {
 		return ResponseEntity.status(status).body(getErrorAttributes(request, true));
 	}
 
+	/**
+	 * Extract the error message from the request context.
+	 *
+	 * @param context the RequestContext
+	 * @return String the error message
+	 */
+	String getErrorMessage(final RequestContext context) {
+		String errorMessage = "Unexpected error occurred";
+
+		InputStream inputStream = null;
+		try {
+			inputStream = context.getResponseDataStream();
+			if (inputStream != null) {
+				try {
+					errorMessage = IOUtils.toString(inputStream, StandardCharsets.UTF_8.name());
+					LOGGER.error("Error Response:: {}", errorMessage);
+				} catch (final IOException e) {
+					LOGGER.error("IOException:: {}", e);
+				}
+			}
+		} finally {
+			IOUtils.closeQuietly(inputStream);
+		}
+
+		return errorMessage;
+	}
+
+	/**
+	 * Retrieve the error attributes from the request.
+	 *
+	 * @param request the HttpServletRequest
+	 * @param includeStackTrace a boolean determining if the stacktrace should be included in the attributes or not
+	 * @return Map&lt;String, Object&gt; the error attributes
+	 */
 	@Override
 	public Map<String, Object> getErrorAttributes(final HttpServletRequest request, final boolean includeStackTrace) {
 		final RequestAttributes requestAttributes = new ServletRequestAttributes(request);
--- src/test/java/gov/va/ascent/gateway/error/AscentGatewayErrorControllerTest.java
+++ src/test/java/gov/va/ascent/gateway/error/AscentGatewayErrorControllerTest.java
@@ -1,16 +1,30 @@
 package gov.va.ascent.gateway.error;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,10 +35,16 @@ import org.springframework.web.context.request.RequestAttributes;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.netflix.zuul.context.RequestContext;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AscentGatewayErrorControllerTest {
+
+	private static final String ERROR_MSG = "This is a test error message";
 
 	@Mock
 	ErrorAttributes errorAttributes = new ErrorAttributes() {
@@ -45,22 +65,80 @@ public class AscentGatewayErrorControllerTest {
 	@Autowired
 	private TestRestTemplate testRestTemplate;
 
-	@Mock
-	HttpServletResponse response;
+//	@Mock
+//	HttpServletResponse response;
 
 	@InjectMocks
+//	@Spy
 	AscentGatewayErrorController controller;
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		Logger logger = null;
+		try {
+			final Field loggerField = AscentGatewayErrorController.class.getDeclaredField("LOGGER");
+			loggerField.setAccessible(true);
+			logger = (Logger) loggerField.get(controller);
+			logger.setLevel(Level.DEBUG);
+		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+			e.printStackTrace();
+			fail("Could not set log level to DEBUG");
+		}
+		// ((ch.qos.logback.classic.Logger) AscentGatewayErrorController.LOGGER).setLevel(ch.qos.logback.classic.Level.DEBUG);
+		;
+	}
 
 	@Test
 	public void errorTest() {
 		String result = testRestTemplate.getForObject("/error", String.class);
+		System.out.println(">>>>>    EntityResponse value:  " + result);
 		final JsonParser parser = new JsonParser();
 		final JsonElement jsonElement = parser.parse(result);
 		assertNotNull(jsonElement.isJsonObject());
 
-		doReturn(0).when(response).getStatus();
+		final HttpServletResponse spiedResponse = spy(HttpServletResponse.class);
+		doReturn(0).when(spiedResponse).getStatus();
+//		spy(controller).error(any(HttpServletRequest.class), any(HttpServletResponse.class));
+		mock(AscentGatewayErrorController.class).error(any(), any());
+//		doReturn(0).when(response).getStatus();
+		// when(response.getStatus()).thenReturn(0);
 		result = testRestTemplate.getForObject("/error", String.class);
-
+		System.out.println(">>>>>    EntityResponse value:  " + result);
+		assertNotNull(result);
 	}
 
+	@Test
+	public void testGetErrorMessage() {
+		final RequestContext context = mock(RequestContext.class);
+
+		InputStream is = null;
+		try {
+			is = spy(new ByteArrayInputStream(ERROR_MSG.getBytes()));
+			when(context.getResponseDataStream()).thenReturn(is);
+
+			final String errorMessage = controller.getErrorMessage(context);
+			assertNotNull(errorMessage);
+			assertTrue(ERROR_MSG.equals(errorMessage));
+		} finally {
+			IOUtils.closeQuietly(is);
+		}
+
+		try {
+			is = spy(new ByteArrayInputStream(ERROR_MSG.getBytes()));
+			when(context.getResponseDataStream()).thenReturn(is);
+			try {
+				// doThrow(new IOException("Testing")).when(is).read(any(byte[].class), anyInt(), anyInt());
+				doThrow(new IOException("Testing")).when(is).read(any(byte[].class));
+			} catch (final IOException e) {
+				e.printStackTrace();
+				fail("Should not havae thrown exception here.");
+			}
+			final String errorMessage = controller.getErrorMessage(context);
+			assertNotNull(errorMessage);
+			assertTrue(ERROR_MSG.equals(errorMessage));
+		} finally {
+			IOUtils.closeQuietly(is);
+		}
+	}
 }
